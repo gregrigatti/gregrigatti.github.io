316623166f2834f552763f3b933c3a9ac320d116
Greg <lamik76@hotmail.com>
2022-01-20 21:30:02 -0500

    Correction to localStorage saving

--- JS/main.v2.js
+++ JS/main.v2.js
@@ -93,7 +93,12 @@ btns = {
 			three:{prompt:allPrompts[2].promptValue, input:document.getElementById("text3").value}
 			//three:{prompt:allPrompts[2].promptValue, input:allPrompts[2].textBox.value} why doesn't allPrompts[2]textBox.value work
 		};
-		localStorage['inputcache'] = JSON.stringify(objectsToSave);
+		//localStorage['inputcache'] = JSON.stringify(objectsToSave);
+		//how to use a variable to differentiate submission names?
+		window.localStorage.setItem("Submission", JSON.stringify(objectsToSave));
+		//use these two lines for retrieving: 
+		//let newObject = window.localStorage.getItem("Submission");
+		//console.log(JSON.parse(newObject))
 		console.log('done', JSON.stringify(objectsToSave));
 	}
 }
