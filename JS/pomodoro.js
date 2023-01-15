096164190bca3b3e2a9e8bde3729e7857403cec8
Greg R <gregoryrigatti@gmail.com>
2023-01-15 13:55:33 -0500

    progress on pomodoro page 1.15.2023

--- JS/pomodoro.js
+++ JS/pomodoro.js
@@ -1,4 +1,8 @@
-var element = document.querySelector('.chart');
+var element = document.querySelector(".chart");
 new EasyPieChart(element, {
-    size: 200
+    size: 200,
+    barColor: "DarkGreen",
+    onStep: function(from, to, percent) {
+        this.el.children[0].innerHTML = Math.round(percent);
+    }
 });
